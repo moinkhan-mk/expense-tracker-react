f08849e761828760733346c1f545633779a761e4
Moin Khan <moin.khan.mk6600@gmail.com>
2020-06-21 18:38:10 +0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React, {useContext} from 'react'
 export const Header = () => {
     return (
         <h2>
-            Expense Tracker
+            Expense Tracker 1
         </h2>
     )
 }
